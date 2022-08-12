ac4f5190a2875e1456e32c82da94be484a8347d8
Konstantin7898 <lexus7898@gmail.com>
2022-08-12 21:07:15 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,40 @@
-var throttle = require('lodash.throttle');
-var iframe = document.querySelector('iframe');
-var player = new Vimeo.Player(iframe);
+// var throttle = require('lodash.throttle');
+// var iframe = document.querySelector('iframe');
+// var player = new Vimeo.Player(iframe);
 
-const lastSavedTime = localStorage.getItem('videoplayer-current-time');
-console.log(lastSavedTime);
+// const lastSavedTime = localStorage.getItem('videoplayer-current-time');
+// console.log(lastSavedTime);
 
-if (lastSavedTime) {
-  player.setCurrentTime(lastSavedTime);
-}
+// if (lastSavedTime) {
+//   player.setCurrentTime(lastSavedTime);
+// }
 
-player.on(
-  'timeupdate',
-  throttle(function (timeObj) {
-    localStorage.setItem(
-      'videoplayer-current-time',
-      JSON.stringify(timeObj.seconds)
-    );
-  }, 1000)
-);
+// player.on(
+//   'timeupdate',
+//   throttle(function (timeObj) {
+//     localStorage.setItem(
+//       'videoplayer-current-time',
+//       JSON.stringify(timeObj.seconds)
+//     );
+//   }, 1000)
+// );
+
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', async () => {
+  if (localStorage.getItem('currentVideoTime')) {
+    await player.setCurrentTime(+localStorage.getItem('currentVideoTime'));
+    localStorage.removeItem('currentVideoTime');
+  }
+});
+
+window.addEventListener('beforeunload', async () => {
+  try {
+    const curTime = await player.getCurrentTime();
+    localStorage.setItem('currentVideoTime', curTime);
+  } catch (err) {
+    console.log(err);
+  }
+});
