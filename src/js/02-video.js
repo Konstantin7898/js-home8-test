4d67e77e00e425fc962412e9aa6053e826372477
Konstantin7898 <lexus7898@gmail.com>
2022-08-12 22:47:41 +0300

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -19,23 +19,3 @@ player.on(
     );
   }, 1000)
 );
-
-// import Player from '@vimeo/player';
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
-
-// player.on('play', async () => {
-//   if (localStorage.getItem('currentVideoTime')) {
-//     await player.setCurrentTime(+localStorage.getItem('currentVideoTime'));
-//     localStorage.removeItem('currentVideoTime');
-//   }
-// });
-
-// window.addEventListener('beforeunload', async () => {
-//   try {
-//     const curTime = await player.getCurrentTime();
-//     localStorage.setItem('currentVideoTime', curTime);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
